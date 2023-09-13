446918bbeb0e4f09ad39b15f641b0b76b788e1a4
eugenio <eaof@icomp.ufam.edu.br>
2023-09-13 17:04:42 -0400

    Resposta para a questao 04

---
+++ prova01/questao04/script.js
@@ -0,0 +1,12 @@
+function green(){
+    console.log("verde");
+}
+function red(){
+    console.log("vermelho");
+}
+function blue(){
+    console.log("azul");
+}
+function reset(){
+    console.log("reset");
+}
