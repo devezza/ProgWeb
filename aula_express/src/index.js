3736a2f6e8f75173944ffc75c245344a6ff274b6
Deveza <mfdl@icomp.ufam.edu.br>
2023-09-20 17:44:05 -0400

    Aula de express

---
+++ aula_express/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use("/img", [express.static(`${__dirname}/../public/img`)
+]);
+
+app.use((req,res)=>{
+    res.end("Acesse o arquivo /img/maca.png");
+});
+
+app.listen(PORT, () =>{
+    console.log(`Express app iniciado na porta ${PORT}`);
+});
